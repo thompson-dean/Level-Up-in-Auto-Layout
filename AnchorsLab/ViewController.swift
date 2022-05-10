2a41816a28e45ed31ce7b1f7bf60172acecc5d95
Dean Thompson <deanthompson.music@gmail.com>
2022-05-10 21:30:17 +0900

    Added upper left and right button

--- AnchorsLab/ViewController.swift
+++ AnchorsLab/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        setupViews()
     }
-
-
+    
+    func setupViews() {
+        
+        let upperLeftLabel = makeLabel(withText: "upperLeft")
+        let upperRightLabel = makeLabel(withText: "upperRight")
+        
+        view.addSubview(upperLeftLabel)
+        view.addSubview(upperRightLabel)
+        
+        NSLayoutConstraint.activate([
+            upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            
+            upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            view.trailingAnchor.constraint(equalTo: upperRightLabel.trailingAnchor, constant: 8cd)
+            
+        ])
+        
+    }
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        
+        return label
+    }
 }
 
